435c629da3b892f749d03b1a8f9dd42db545c94c
Kevin Ramirez <kevnass10@gmail.com>
2026-01-01 21:07:01 -0300

    Gimnasio parte 1/ falta agregar javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+let nombre = document.getElementById("nombre");
+let mail = document.getElementById("mail");
+let texto = document.getElementById("texto");
+let enviar = document.getElementById("enviar");
+let error = document.createElement("p")
+
+error.innerHTML="Error"
+
+enviar.addEventListener("click", ()=>{
+    nombre.value 
+    if(nombre.value===""){
+       error
+    }})
+        
