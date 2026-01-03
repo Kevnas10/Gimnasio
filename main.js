f1054a16470139880d4d44fb42a29531010ec446
Kevin Ramirez <kevnass10@gmail.com>
2026-01-03 20:56:23 -0300

    landing complet scroll and  validacion

--- main.js
+++ main.js
@@ -2,13 +2,43 @@ let nombre = document.getElementById("nombre");
 let mail = document.getElementById("mail");
 let texto = document.getElementById("texto");
 let enviar = document.getElementById("enviar");
-let error = document.createElement("p")
+let formulario = document.getElementById("formulario");
+let error1 = document.getElementById("text-de-error1")
+let error2 = document.getElementById("text-error2")
+let button = document.getElementById("button")
 
-error.innerHTML="Error"
+enviar.addEventListener("click",()=>{
+if(nombre.value === ""){
+   error1.style.display= "block"
+}else{
+   error1.style.display="none"
+}
+})
 
 enviar.addEventListener("click", ()=>{
-    nombre.value 
-    if(nombre.value===""){
-       error
-    }})
-        
+if(mail.value=== ""){
+   error2.style.display="block"
+}else if(
+   mail.value.indexOf("@") === -1 || 
+   mail.value.indexOf(".") === -1 
+){
+   error2.style.display="block"
+}else{
+   error2.style.display= "none"
+}
+})
+
+window.addEventListener("scroll", ()=>{ 
+  if(window.scrollY > 300){
+   button.style.display="block"
+  } else{
+   button.style.display= "none"
+  }
+})
+
+button.addEventListener("click", ()=>{
+   window.scrollTo({
+      top: 0,
+      behavior:"smooth" 
+   })
+})
